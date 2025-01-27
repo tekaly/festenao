e10ba22422f980597fce6185ff6b468b542db21a
Alexandre Roux <alex@tekartik.com>
2025-01-27 11:18:29 +0100

    metas screen

---
+++ packages_flutter/festenao_admin_base_app/lib/screen/admin_metas_screen.dart
@@ -0,0 +1,62 @@
+import 'package:festenao_admin_base_app/admin_app/admin_app_project_context.dart';
+import 'package:festenao_admin_base_app/layout/admin_screen_layout.dart';
+import 'package:festenao_admin_base_app/screen/admin_meta_general_edit_screen.dart';
+import 'package:festenao_admin_base_app/screen/screen_import.dart';
+import 'package:festenao_admin_base_app/view/entry_tile.dart';
+import 'package:festenao_common/text/text.dart';
+
+import 'package:tekartik_app_rx_bloc/auto_dispose_state_base_bloc.dart';
+
+class AdminMetaScreenBlocState {
+  AdminMetaScreenBlocState();
+}
+
+class AdminMetasScreenBloc
+    extends AutoDisposeStateBaseBloc<AdminMetaScreenBlocState> {
+  final FestenaoAdminAppProjectContext projectContext;
+  AdminMetasScreenBloc({required this.projectContext});
+}
+
+class AdminMetasScreen extends StatefulWidget {
+  const AdminMetasScreen({super.key});
+
+  @override
+  State<AdminMetasScreen> createState() => _AdminMetasScreenState();
+}
+
+class _AdminMetasScreenState extends State<AdminMetasScreen> {
+  @override
+  Widget build(BuildContext context) {
+    var bloc = BlocProvider.of<AdminMetasScreenBloc>(context);
+    return AdminScreenLayout(
+      appBar: AppBar(
+        title: const Text('Metas'),
+      ),
+      body: ValueStreamBuilder<AdminMetaScreenBlocState>(
+        stream: bloc.state,
+        builder: (context, snapshot) {
+          return ListView(
+            children: [
+              EntryTile(
+                label: textMetaGeneral,
+                onTap: () {
+                  goToAdminMetaGeneralEditScreen(context,
+                      projectContext: bloc.projectContext);
+                },
+              )
+            ],
+          );
+        },
+      ),
+    );
+  }
+}
+
+Future<void> goToAdminMetasScreen(BuildContext context,
+    {required FestenaoAdminAppProjectContext projectContext}) async {
+  await Navigator.of(context).push<void>(MaterialPageRoute(
+      builder: (_) => BlocProvider(
+          blocBuilder: () =>
+              AdminMetasScreenBloc(projectContext: projectContext),
+          child: const AdminMetasScreen())));
+}
---
+++ packages_flutter/festenao_admin_base_app/lib/view/entry_tile.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class EntryTile extends StatefulWidget {
+  final String? label;
+  final String? value;
+  final VoidCallback? onTap;
+  final VoidCallback? onLongPress;
+  const EntryTile(
+      {super.key, this.label, this.value, this.onTap, this.onLongPress});
+
+  @override
+  State<EntryTile> createState() => _EntryTileState();
+}
+
+class _EntryTileState extends State<EntryTile> {
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(widget.label ?? '-- no label --'),
+      subtitle: Text(widget.value ?? ''),
+      onTap: widget.onTap,
+      onLongPress: widget.onLongPress,
+      trailing: const Icon(Icons.navigate_next),
+    );
+  }
+}
