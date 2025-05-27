72ad1fb7e5d2329fd970c65d58b9a11530253941
Alexandre Roux <alex@tekartik.com>
2025-05-27 12:26:08 +0200

    feat: add imports

---
+++ packages/festenao_common/lib/festenao_cv.dart
@@ -0,0 +1,1 @@
+export 'package:cv/cv_json.dart';
---
+++ packages/festenao_common/lib/festenao_http.dart
@@ -0,0 +1,3 @@
+export 'package:tekartik_app_http/app_http.dart';
+export 'package:tekartik_http/http_memory.dart';
+export 'package:tekartik_http/http_utils.dart';
