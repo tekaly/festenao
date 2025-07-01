9dbc6d938bf6bf0c1f65523952b4ab69cc277fda
Alexandre Roux <alex@tekartik.com>
2025-07-01 18:30:17 +0200

    debug server test

--- packages/festenao_common/test/festenao_test_server_test.dart
+++ packages/festenao_common/test/festenao_test_server_test.dart
@@ -1,5 +1,9 @@
+// ignore: unused_import
+import 'package:festenao_common/festenao_api.dart';
 import 'package:festenao_common/test/festenao_test_server_test.dart';
+import 'package:tekartik_common_utils/common_utils_import.dart';
 
 Future<void> main() async {
+  // debugWebServices = devWarning(true);
   testFestenaoServerGroup(initFestenaoAllMemory);
 }
