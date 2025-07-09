3f7a3f123003d2b52c877b90a7cf2f14ea99ac97
Alexandre Roux <alex@tekartik.com>
2025-07-09 16:59:44 +0200

    add web splash

--- packages_flutter/festenao_common_flutter/lib/common_utils_widget.dart
+++ packages_flutter/festenao_common_flutter/lib/common_utils_widget.dart
@@ -9,5 +9,6 @@ export 'package:tekartik_app_flutter_widget/view/tile_padding.dart';
 export 'package:tekartik_app_navigator_flutter/content_navigator.dart';
 // Ui builder
 export 'package:tekartik_app_rx_utils/app_rx_utils.dart';
+export 'package:tekartik_web_splash/web_splash.dart';
 
 export 'common_utils_flutter.dart';
