63ca7fc35d1e5167f9d36022752c68d89cd9a126
Alexandre Roux <alex@tekartik.com>
2025-12-24 17:02:59 +0100

    feat: starting dashboard

---
+++ packages/festenao_common/lib/firebase/firebase_rest.dart
@@ -0,0 +1,4 @@
+export 'package:tekartik_firebase_auth_rest/auth_rest.dart';
+export 'package:tekartik_firebase_firestore_rest/firestore_rest.dart';
+export 'package:tekartik_firebase_rest/firebase_rest.dart';
+export 'package:tekartik_firebase_storage_rest/storage_rest.dart';
