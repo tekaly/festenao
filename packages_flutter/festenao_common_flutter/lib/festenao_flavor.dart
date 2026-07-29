50f6e1df8c24df2ee1eaa8a7c441b05ee0951087
Alexandre Roux <alex@tekartik.com>
2026-07-29 18:23:27 +0200

    feat: add festenao_flavor.dart for default flavor context configuration

---
+++ packages_flutter/festenao_common_flutter/lib/festenao_flavor.dart
@@ -0,0 +1,8 @@
+import 'package:festenao_common/festenao_flavor.dart';
+import 'package:flutter/foundation.dart';
+export 'package:festenao_common/festenao_flavor.dart';
+
+/// Default flavor context, either base or uri for the web or dev by default
+final festenaoFlavorContextDefault = kIsWeb
+    ? tkCmsFlavorContextFromUri(Uri.base)
+    : FlavorContext.dev;
